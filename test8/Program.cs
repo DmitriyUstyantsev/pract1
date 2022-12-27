0c4f5d3f8405f07193ed0bccf9d40b61f96af572
DUst <udv@ec-asu.ru>
2022-12-27 15:51:48 +0500

    Добавлено ДЗ практикум 3

---
+++ test8/Program.cs
@@ -0,0 +1,27 @@
+﻿// Напишите программу, которая принимает на вход пятизначное число и проверяет, является ли оно палиндромом.
+// 14212 -> нет
+// 12821 -> да
+// 23432 -> да
+
+
+Console.Write("Введите число для проверки на полиндром : ");
+ int number = int.Parse(Console.ReadLine());
+            int i, sum = 0;
+            int temp = number;
+            while (number > 0)
+            {
+              
+                i = number % 10;
+             
+                sum = (sum * 10) + i;
+             
+                number = number / 10; 
+            }
+            if (temp == sum)
+            
+Console.WriteLine($"Число {temp} палиндром.");
+
+else
+
+Console.WriteLine($"Число {temp} не палиндром");
+

802aca098367472eb4a0e4f4c806cb31344c32d0
DUst <udv@ec-asu.ru>
2023-01-17 15:40:09 +0500

    new home work 19 jan

---
+++ test11/Program.cs
@@ -0,0 +1,26 @@
+﻿//Задача 25: Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B. (без использования Math.Pow)
+// 3, 5 -> 243 (3⁵)
+// 2, 4 -> 16
+
+int A = ReadInt("Введите число A: ");
+int B = ReadInt("Введите число B: ");
+ToDegree(A, B);
+
+
+// Функция возведения в степень
+void ToDegree(int a, int b)
+{
+    int res = 1;
+    for (int i = 1; i <= b; i++)
+    {
+        res = res * a;
+    }
+    Console.WriteLine(res);
+}
+
+// Функция ввода
+int ReadInt(string message)
+{
+    Console.WriteLine(message);
+    return Convert.ToInt32(Console.ReadLine());
+}
